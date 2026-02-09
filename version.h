865657a7aa01b06e864f0cdef2ce2928e50f3ff2
°⊥⋊ɹ∀° <arktiothome@gmail.com>
2026-02-09 07:35:48 +0530

    CI: Enforce Signed binary only and enhance release notes with checksums

--- version.h
+++ version.h
@@ -2,4 +2,4 @@
 #define VERSION_W L"6.0.7"
 #define VERSION_CSV 6,0,7,0
 #define VERSION_XML "6.0.7.0"
-#define DOWNLOAD_URL L"https://github.com/ArKT-7/QuickRotate/releases/latest/download/QuickRotate_Signed.exe"
+#define DOWNLOAD_URL L"https://github.com/ArKT-7/QuickRotate/releases/latest/download/QuickRotate.exe"
